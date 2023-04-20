6a2c01d2b124c23829f39c2e26463cb1ce59b4f7
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2023-04-21 01:43:03 +0200

    add slider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,29 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery
+//= require slick
+
+
+
+function initializeSlider() {
+  $('.slider:not(.slick-initialized)').slick({
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+}
+
+function initialize() {
+  initializeSlider();
+}
+
+document.addEventListener("turbo:load", function() {
+  initialize()
+
+  $(document).ready(function() {
+    // Find the prev and next buttons and remove them
+    $('.prev, .next').remove();
+  });
+  
+})
