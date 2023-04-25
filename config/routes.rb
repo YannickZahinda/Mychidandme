9146335c244055b7154e8494e45c323457021afa
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2023-04-25 14:28:03 +0200

    add devise and first blog actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   root to: "home#index"
 
+  namespace :admin do
+    resource :blog, only: [:index, :new, :show, :create, :update, :delete, :edit]
+  end
+
   resource :home, only: [:index]
+  resources :blogs, only: [:index, :show]
 end
