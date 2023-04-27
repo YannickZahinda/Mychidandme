a5156ea26bd3f78f1694f179ec99896fae36297c
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2023-04-27 14:01:51 +0200

    add blog delete option

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @blogs = Blog.all
     @impacts = Impact.all
-    @testimonials = Testimony.all
+    @testimonies = Testimony.all
   end
 end
