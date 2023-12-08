e758f622265c15d5e3ca24386912c875d1e14c52
christian <christianbiringanine22@gmail.com>
2023-12-08 14:12:13 +0200

    Addt the lister to toggle the navigation menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -52,3 +52,12 @@ document.addEventListener("turbo:load", function() {
     });  
   });
 })
+
+// Collapsible navbar
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) => 
+  item.addEventListener("click", function() {
+    this.classList.toggle("collapsible--expanded")
+  })
+)
