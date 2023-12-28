727da4982d1c77c504e5d8e0c55dd9c171f49d8a
christian <christianbiringanine22@gmail.com>
2023-12-28 16:16:16 +0200

    IMplement the nav bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -21,37 +21,37 @@ function initialize() {
   initializeSlider();
 }
 
-document.addEventListener("turbo:load", function() {
-  initialize();
-  $(document).ready(function () {
-    var trigger = $('.hamburger'),
-        overlay = $('.overlay'),
-       isClosed = false;
+// document.addEventListener("turbo:load", function() {
+//   initialize();
+//   $(document).ready(function () {
+//     var trigger = $('.hamburger'),
+//         overlay = $('.overlay'),
+//        isClosed = false;
   
-      trigger.click(function () {
-        hamburger_cross();      
-      });
+//       trigger.click(function () {
+//         hamburger_cross();      
+//       });
   
-      function hamburger_cross() {
+//       function hamburger_cross() {
   
-        if (isClosed == true) {          
-          overlay.hide();
-          trigger.removeClass('is-open');
-          trigger.addClass('is-closed');
-          isClosed = false;
-        } else {   
-          overlay.show();
-          trigger.removeClass('is-closed');
-          trigger.addClass('is-open');
-          isClosed = true;
-        }
-    }
+//         if (isClosed == true) {          
+//           overlay.hide();
+//           trigger.removeClass('is-open');
+//           trigger.addClass('is-closed');
+//           isClosed = false;
+//         } else {   
+//           overlay.show();
+//           trigger.removeClass('is-closed');
+//           trigger.addClass('is-open');
+//           isClosed = true;
+//         }
+//     }
     
-    $('[data-toggle="offcanvas"]').click(function () {
-          $('#wrapper').toggleClass('toggled');
-    });  
-  });
-})
+//     $('[data-toggle="offcanvas"]').click(function () {
+//           $('#wrapper').toggleClass('toggled');
+//     });  
+//   });
+// })
 
 // Collapsible navbar
 
